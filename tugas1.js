bd909e6fbb22c426366567e6b9c437e56fa4b4a8
Mahansa <hansagamers11@gmail.com>
2024-11-12 17:18:20 +0700

    Add files via upload

---
+++ tugas1.js
@@ -0,0 +1,11 @@
+let siswa = {
+    nama: "Mahansa",
+    kelas:"10 RPL",
+    nilai: {
+        Matematika: 80,
+        BahasaIndonesia: 85,
+        BahasaInggris: 100,
+        Ipa: 90
+    }
+}
+console.log(siswa)
---
+++ tugas2.js
@@ -0,0 +1,5 @@
+let array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+console.log("Data ketiga dalam array:", array[3]);
+array.push(21); 
+array.shift();
+console.log("Array hasil setelah perubahan:", array);
